9705c1e38b18a4d21a4ed8e4c913635302537238
Helen <31750014+hxgreensoul@users.noreply.github.com>
2021-03-11 17:49:20 -0800

    Added Colliders, Ridid Bodies, DetectCollision Script

---
+++ Assets/Scripts/DetectCollisions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisions : MonoBehaviour
+{
+    //allow for collider and triggers to work
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -10,18 +10,23 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //calls-runs function, seconds, how fast generate animal
+        InvokeRepeating("SpawnRandomAnimal", 3.0f, 1.5f); 
         
     }
 
     // Update is called once per frame
     void Update() // happens only when needed, random - computational pwr
-    {  
-        if(Input.GetKeyDown(KeyCode.A))
-        {
-            float randXPos = Random.Range(-xPosRange, xPosRange);
-            int animalPrefabIndex = Random.Range(0, animalPrefabs.Length); //generates different animals
-            Vector3 randPos = new Vector3(randXPos, 0, 21);
-            Instantiate(animalPrefabs[animalPrefabIndex], randPos, animalPrefabs[animalPrefabIndex].transform.rotation);
-        }
+    {
+
+ 
+    }
+    //declares a function
+    void SpawnRandomAnimal()
+    {
+        float randXPos = Random.Range(-xPosRange, xPosRange);
+        int animalPrefabIndex = Random.Range(0, animalPrefabs.Length); //generates different animals
+        Vector3 randPos = new Vector3(randXPos, 0, 21);
+        Instantiate(animalPrefabs[animalPrefabIndex], randPos, animalPrefabs[animalPrefabIndex].transform.rotation);
     }
 }
